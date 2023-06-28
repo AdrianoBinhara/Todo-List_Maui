a4e1e26b4d1e09eaa3871184016b232418d46a42
Gabriel Sales <ghe_sales@hotmail.com>
2023-06-28 20:17:57 -0300

    Adicionar config para IOS

--- Maui-Fun/Maui-Fun/Platforms/iOS/AppDelegate.cs
+++ Maui-Fun/Maui-Fun/Platforms/iOS/AppDelegate.cs
@@ -1,10 +1,14 @@
 ﻿using Foundation;
+using SQLitePCL;
 
 namespace Maui_Fun;
 
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp()
+    {
+        raw.SetProvider(new SQLite3Provider_sqlite3());
+        return MauiProgram.CreateMauiApp();
+    }
 }
-
